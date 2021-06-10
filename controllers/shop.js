e32caf1522727ff43efc29276b3ae5c3d484ffd1
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-06-10 22:36:56 +0530

    prettify

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,65 +1,65 @@
-const Product = require('../models/product');
-const Cart = require('../models/cart');
+const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
       prods: products,
-      pageTitle: 'All Products',
-      path: '/products',
+      pageTitle: "All Products",
+      path: "/products",
       hasProducts: products.length > 0,
       activeShop: true,
-      productCSS: true
+      productCSS: true,
     });
   });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
       product: product,
       pageTitle: product.title,
-      path: '/products'
+      path: "/products",
     });
   });
-}
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
+      pageTitle: "Shop",
+      path: "/",
     });
   });
-}
+};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price)
-  })
-  res.redirect('/cart');
-}
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    pageTitle: 'Your Cart',
-    path: '/cart'
-  })
-}
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  })
-}
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  })
-}
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -26,20 +26,20 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString()
-    getProductsFromFile(products => {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
   }
 
   static findById(id, cb) {
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       const product = products.find((p) => p.id === id);
       cb(product);
-    })
+    });
   }
 
   static fetchAll(cb) {
