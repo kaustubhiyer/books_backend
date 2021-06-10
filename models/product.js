75860b1bf931c9aee63d01bedc64a64b1c024185
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-06-10 20:22:53 +0530

    added more product customizations

--- models/product.js
+++ models/product.js
@@ -18,8 +18,11 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
+  constructor(t, imageUrl, description, price) {
     this.title = t;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
