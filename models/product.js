3b8d2fb594cd2e140e6521271531a19c5650e814
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-06-10 23:18:22 +0530

    added edit product to admin

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t, imageUrl, description, price) {
+  constructor(id, t, imageUrl, description, price) {
+    this.id = id;
     this.title = t;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,23 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,10 @@ router.get("/add-product", adminController.getAddProduct);
 
 router.get("/products", adminController.getProducts);
 
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
 
