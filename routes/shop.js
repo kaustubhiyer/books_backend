8ea83a6216bfa6b201bf650393e1f32ac66fd7bc
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-06-10 20:03:37 +0530

    added new feature routes

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,16 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const productsController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', productsController.getIndex);
+
+router.get('/products', productsController.getProducts);
+
+router.get('/cart', productsController.getCart);
+
+router.get('/checkout', productsController.getCheckout);
 
 module.exports = router;
